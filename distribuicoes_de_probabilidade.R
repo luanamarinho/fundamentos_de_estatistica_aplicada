2f0c5765c391bb393da92d9f9887ef92872bf44d
Luana Marinho <luana.marinho@gmail.com>
2017-07-04 18:04:42 -0300

    Create distribuicoes_de_probabilidade.R

---
+++ distribuicoes_de_probabilidade.R
@@ -0,0 +1,9 @@
+## Distribuição normal ou Gaussiana
+
+# É notadamente a mais importante dentre todos os tipos de distribuição de variáveis aleatórias contínuas.
+# Suas curvas são *simétricas*, *unimodais*, seguem a forma de um *sino" e representam muitos processos naturais e sociais (dar exemplos).
+# Seus parâmetros descritivos são a média e o desvio padrão.
+# A média pode deslocar a curva para a direita ou esquerda (na reta dos reais, por exemplo), enquanto o desvio-padrão a comprime ou estende (quanto menor, mais comprimida; quanto maior, mais estendida).
+# A distribuição normal com média 0 e desvio 1 é chamada de "standard normal distribution" ou, em português, "distribuição normal padrão", "distribuição normal padronizada" ou "distribuição normal standarizada". 
+# Em verdade, em nenhuma das situações práticas a variável é perfeitamente representada por uma distribuição normal, mas apenas aproximadamente. Contudo, ainda assim essa distribuição é extremamente útil para se modelar diversos fenômenos.
+# Variável Z:
